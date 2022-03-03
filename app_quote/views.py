e63f6104adcec44f2b6a8944ff6075ee25e8da3e
Alamansson <alamanov.dev@gmail.com>
2022-03-03 14:19:46 +0600

    app_quote

---
+++ app_quote/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Quote
+from .serializers import QuoteSerializer
+
+
+class QuoteViewSet(ModelViewSet):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
+
+
